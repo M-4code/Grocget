45f9d00ca8610dc2679f8f9bc5435f8c26a561b3
manish <manish.kumar13@boeing.com>
2020-06-13 18:43:02 +0530

    Added DTO layer

---
+++ GrocGet.API/Controllers/ProductController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GrocGet.DTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GrocGet.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<Product> Get()
+        {
+            return new Product() { Id = 1, Name = "Vegetabl Oil", ProdCatId = 1, Price = 203.50 };
+        }
+    }
+        
+
+}
